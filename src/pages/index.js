5490b8819ab4cb32cfb05c0cd061cfb989a033bc
AlexTicovschi <alexmticovschi@gmail.com>
2019-09-27 18:58:47 +0100

    querying firebase data from Gatsby with GraphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,30 +5,37 @@ import Layout from "../components/layout"
 // import Image from "../components/image"
 // import SEO from "../components/seo"
 
-const IndexPage = (props) => {
-  console.log(props);
+const IndexPage = props => {
+  const { allArticle } = props.data;
   return (
     <Layout>
-      Blog
+      {allArticle.edges.map(article => (
+        <div key={article.node.id}>
+          <h2> {article.node.title} - <small>{article.node.author.name}</small></h2>
+          <div>
+            {article.node.summary}
+          </div>
+        </div>
+      ))}
     </Layout>
-  );
+  )
 }
 
 export const query = graphql`
-{
-  allArticle {
-    edges {
-      node {
-        summary
-        title
-        id
-        author {
-          name
+  {
+    allArticle {
+      edges {
+        node {
+          summary
+          title
+          id
+          author {
+            name
+          }
         }
       }
     }
   }
-}
-`;
+`
 
 export default IndexPage
