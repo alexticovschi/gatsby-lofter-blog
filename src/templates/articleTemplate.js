ead7e46c2e8c3cf822db258907303ebc58859310
AlexTicovschi <alexmticovschi@gmail.com>
2019-10-01 18:34:27 +0100

    updating Layout component

--- src/templates/articleTemplate.js
+++ src/templates/articleTemplate.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Layout from "../components/layout"
 
-const ArticleTemplate = () => {
+const ArticleTemplate = (props) => {
   return (
-    <>
-      <div>Article Page</div>
+    <Layout>
+      <div>{props.pageContext.title}</div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae necessitatibus accusantium eveniet. Sunt, doloremque iure. Laboriosam repellat ratione quidem, dolores unde id, necessitatibus mollitia earum tempore ex ab, architecto deserunt eos magni sit reprehenderit excepturi dolorem explicabo voluptates incidunt provident minima doloremque cum? Ducimus dolor blanditiis dolore. Odit voluptate pariatur, eos corrupti hic suscipit at.</p>
-    </>
+    </Layout>
   )
 }
 
