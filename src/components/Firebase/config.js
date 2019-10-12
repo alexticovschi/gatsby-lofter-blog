49f91422f4156d0f50de3909c3e1f43996b8bab6
AlexTicovschi <alexmticovschi@gmail.com>
2019-10-12 11:39:34 +0100

    adding firebase boilerplate

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDQie81wCQ3nGoQqaR3ZUzzbzFSsorrbwU",
+    authDomain: "gatsby-lofter-blog.firebaseapp.com",
+    databaseURL: "https://gatsby-lofter-blog.firebaseio.com",
+    projectId: "gatsby-lofter-blog",
+    storageBucket: "gatsby-lofter-blog.appspot.com",
+    messagingSenderId: "1038892158518",
+    appId: "1:1038892158518:web:9b027109444db4d61cd297",
+    measurementId: "G-PTKGZYG1ET"
+};
+
+export default firebaseConfig;
